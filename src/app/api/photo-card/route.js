7e72e1635e2a6ba1eedf2837f702ba7561e2c8a3
Hyunji An <hyunji0322123@gmail.com>
2024-06-25 23:23:47 +0900

    update random

--- src/app/api/photo-card/route.js
+++ src/app/api/photo-card/route.js
@@ -1,7 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import { fetchPhotoCard } from "../../../../data/firestore";
 
-export const dynamic = 'auto'
+export const dynamic = "force-dynamic";
 
 export async function GET(request) {
     try {
