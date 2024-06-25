4d2971d30f7a55bd5fc37bb674634ed671098e26
Hyunji An <hyunji0322123@gmail.com>
2024-06-25 23:17:16 +0900

    update random

--- src/app/api/photo-card/route.js
+++ src/app/api/photo-card/route.js
@@ -1,10 +1,11 @@
 import {NextRequest, NextResponse} from "next/server";
 import { fetchPhotoCard } from "../../../../data/firestore";
 
+export const dynamic = 'auto'
+
 export async function GET(request) {
     try {
         const pc_idx = getRandomInt()
-        console.log(pc_idx)
         const img = await fetchPhotoCard(pc_idx);
         if (img === null) {
             return new Response(null, {status: 204})
